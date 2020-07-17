5e7433820c05b9ab1bf8ac33505672572466d363
sharik saifi <shariksaifi786.ss@gmail.com>
2020-07-17 23:48:13 +0530

    AddedBootstrapTheme

---
+++ TheBeginningWeb/Controllers/CartController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TheBeginningWeb.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult Items()
+        {
+            return View();
+        }
+        public ActionResult Checkout()
+        {
+            return View();
+        }
+        public ActionResult Confirmation()
+        {
+            return View();
+        }
+    }
+}
